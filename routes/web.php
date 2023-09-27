b0099d634956de71f08a537e1260ac4483973547
AqilaGhifariWandana <aqila.ghifari.tif22@polban.ac.id>
2023-09-27 10:47:08 +0700

    penambahan dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/dashboard', function () {
-    return view('dahsboard.index');
+    return view('dashboard.index');
 });
 
