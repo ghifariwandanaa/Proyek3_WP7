8336fa77fcbe2493351ce585e40472f24d94bc89
ghifariwandanaa <ghifariwandana@gmail.com>
2023-10-17 08:59:24 +0700

    perubahan format date pada input riwayat pekerjaan

--- resources/views/dashboard/halaman/create.blade.php
+++ resources/views/dashboard/halaman/create.blade.php
@@ -4,7 +4,7 @@
     <div class="pb-3"><a href="{{route('halaman.index')}}"
         class="btn btn-secondary"> Kembali</a>
     </div>
-    <form action="{{route('halaman.store')}}"method="post" enctype="multipart/form-data">
+    <form action="{{route('halaman.store')}}"method="post">
         @csrf
         <div class="mb-3">
           <label for="nama" class="form-label">Nama</label>
@@ -28,13 +28,7 @@
         </div>
         <div class="mb-3">
           <label for="riwayatPekerjaan" class="form-label">Riwayat Pekerjaan</label>
-          <div class="row">
-              <div class="col">
-                  <input type="text" class="form-control form-control sm" name="riwayatPekerjaan[0][tgl_mulai]" placeholder="Tanggal Mulai" required>
-              </div>
-              <div class="col">
-                  <input type="text" class="form-control form-control sm" name="riwayatPekerjaan[0][tgl_akhir]" placeholder="Tanggal Akhir" required>
-              </div>
+            <div class="row">
               <div class="col">
                   <input type="text" class="form-control form-control sm" name="riwayatPekerjaan[0][namaPerusahaan]" placeholder="Nama Perusahaan" required>
               </div>
@@ -44,20 +38,29 @@
               <div class="col">
                   <input type="text" class="form-control form-control sm" name="riwayatPekerjaan[0][jabatan]" placeholder="Jabatan" required>
               </div>
-      </div>      
-        <div class="mb-3">
-          <label for="keahlian" class="form-label">Keahlian</label>
-          <input type="text"class="form-control form-control sm" 
-          name="keahlian" id="keahlian" aria-describedby="helpId" placeholder="Keahlian">
-        </div>
-        <div class="mb-3">
-            <label for="gambar" class="form-label">Gambar</label>
-            <input type="file" class="form-control" name="gambar" id="gambar">
-        </div>
-        <div class="mb-3">
-          <label for="dataDiri" class="form-label">Deskripsi Diri</label>
-          <textarea class="form-control" rows="5" name="dataDiri"></textarea>
-        </div>
-        <button class="btn btn-primary" name="simpan" type="submit">SIMPAN</button>
-    </form>
+            </div>
+        </div>      
+      <div class="mb-3">
+        <div class="row">
+          <div class="col-auto"> Tanggal Mulai </div>
+            <div class="col-auto"><input type="date" class="form-control form-control sm" name="riwayatPekerjaan[0][tgl_mulai]" placeholder="dd/mm/yy" required>
+          </div>
+          <div class="col-auto"> Tanggal Akhir</div>
+            <div class="col-auto"> <input type="date" class="form-control form-control sm" name="riwayatPekerjaan[0][tgl_akhir]" placeholder="dd/mm/yy" required>
+          </div>
+          <div class="mb-3">
+            <label for="keahlian" class="form-label">Keahlian</label>
+            <input type="text"class="form-control form-control sm" 
+            name="keahlian" id="keahlian" aria-describedby="helpId" placeholder="Keahlian">
+          </div>
+          <div class="mb-3">
+              <label for="gambar" class="form-label">Gambar</label>
+              <input type="file" class="form-control" name="gambar" id="gambar">
+          </div>
+          <div class="mb-3">
+            <label for="dataDiri" class="form-label">Deskripsi Diri</label>
+            <textarea class="form-control" rows="5" name="dataDiri"></textarea>
+          </div>
+            <button class="btn btn-primary" name="simpan" type="submit">SIMPAN</button>
+      </form>
 @endsection
