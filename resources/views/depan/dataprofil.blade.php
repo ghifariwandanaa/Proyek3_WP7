7b951ec154cbb83e5c6cac2a520d36fd73a3d685
agistadivab <123919926+agistadivab@users.noreply.github.com>
2023-11-01 12:36:26 +0700

    Update dataprofil.blade.php

--- resources/views/depan/dataprofil.blade.php
+++ resources/views/depan/dataprofil.blade.php
@@ -4,17 +4,12 @@
 <div class="table-responsive">
     <div class="pb-3"><a href="{{route('profile.create')}}"class="btn btn-primary">+ Tambah profile</a></div>
     <table class="table table-stripped">
-        <thead>
-            <th class="col-1">No</th>
-            <th class="col-9">Judul</th>
-            <th class="col-2">Aksi</th>
-        </thead>
         <tbody>
             <?php $i=1;?>
             @foreach ($data as $item)
             <tr>
-                <td class="col-1">{{$i}}</td>
-                <td class="col-3">{{$item->nama}}</td>
+            <td><img src="{{asset('storage/' . $item->gambar) }}" alt="" width="100"></td>
+                <td class="col-9">{{$item->nama}}</td>
                 <td>
                     <a href="{{ route('depan.show', ['depan' => $item->id]) }}" class="btn btn-primary">Show</a>
                     <button class="btn btn-danger" type="submit" name='submit'>Del</button>
