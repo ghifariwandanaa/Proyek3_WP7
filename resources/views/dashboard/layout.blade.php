fb6f445f57e0ee4c572eb99139d3e87017cc2eea
Ahmad Fauzy <fauzy15546@gmail.com>
2023-11-07 20:26:28 +0700

    change front

--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -2,55 +2,47 @@
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Web Profile</title>
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="{{asset('admin')}}/vendors/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="{{asset('admin')}}/vendors/base/vendor.bundle.base.css">
-  <!-- endinject -->
-  <!-- plugin css for this page -->
-  <link rel="stylesheet" href="{{asset('admin')}}/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <link rel="stylesheet" href="{{asset('admin')}}/css/style.css">
-  <!-- endinject -->
-  <link rel="shortcut icon" href="{{asset('admin')}}/images/favicon.png" />
-  @stack('styles')
+  <title>Web Profile</title>
+  <link rel="stylesheet" href="{{ asset('admin') }}/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="{{ asset('admin') }}/vendors/base/vendor.bundle.base.css">
+  <link rel="stylesheet" href="{{ asset('admin') }}/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+  <link rel="stylesheet" href="{{ asset('admin') }}/css/style.css">
+  <link rel="stylesheet" href="welcome/css/styles.css">
+  <link rel="shortcut icon" href="{{ asset('admin') }}/images/favicon.png" />
 </head>
+
 <body>
   <div class="container-scroller">
-    <!-- partial:partials/_navbar.html -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="navbar-brand-wrapper d-flex justify-content-center">
-        <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">  
-          <!-- <a class="navbar-brand brand-logo" href="depan"><img src="{{asset('admin')}}/images/logo.svg" alt="logo"/></a> -->
-          <a class="navbar-brand brand-logo-mini" href="depan"><img src="{{asset('admin')}}/images/logo-mini.svg" alt="logo"/></a>
+        <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
+          <a class="navbar-brand brand-logo-mini" href="depan"><img src="{{ asset('admin') }}/images/logo-mini.svg" alt="logo"/></a>
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
             <span class="mdi mdi-sort-variant"></span>
           </button>
         </div>  
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-      <div class="navbar">
-          <h2 class="style_a">Proyek 3</h2>
-      </div>
+        <div class="navbar">
+          <h2 class="style_a">proyek 3</h2>
+        </div>
         <div class="menu">
-            <tr class>
-                <td><a class="style_a" href="aboutweb">ABOUT</a></li>
-                <td><a class="style_a" href="cv">CV</a></li>
-            </tr>
+          <tr class>
+              <td><a class="style_a" href="/">ABOUT</a></td>
+              <td><a class="style_a" href="cv">CV</a></td>
+          </tr>
         </div>
         <ul class="navbar-nav navbar-nav-right">
           <li class="nav-item nav-profile dropdown">
             <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
-              <img src="{{asset('admin')}}/images/faces/OIP.jpg" alt="profile"/>
+              <img src="{{ asset('admin') }}/images/faces/OIP.jpg" alt="profile"/>
             </a>
             <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-              <a class="dropdown-item" href="{{route('depan.index')}}">
+              <a class="dropdown-item" href="{{ route('depan.index') }}">
                 <i class="mdi mdi-account text-primary"></i>
-                profile
+                Profile
               </a>
               <a class="dropdown-item">
                 <i class="mdi mdi-settings text-primary"></i>
@@ -68,35 +60,33 @@
         </button>
       </div>
     </nav>
-    <!-- partial -->
     <div class="container-fluid page-body-wrapper">
-      <!-- partial:partials/_sidebar.html -->
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
           <li class="nav-item">
-            <a class="nav-link" href="{{route('depan.index')}}">
+            <a class="nav-link" href="{{ route('depan.index') }}">
               <i class="mdi mdi-file-document-box-outline menu-icon"></i>
               <span class="menu-title">Data Profile</span>
             </a>
           </li>            
           <li class="nav-item">
-            <a class="nav-link" href="{{route('profile.index')}}">
+            <a class="nav-link" href="{{ route('profile.index') }}">
               <i class="mdi mdi-file-document-box-outline menu-icon"></i>
               <span class="menu-title">Edit Profil</span>
             </a>
           </li>
         </ul>
       </nav>
-      <!-- partial -->
       <div class="main-panel">
         <div class="content-wrapper">
           @if ($errors->any())
-              <div class="alert alert-danger">
-                <ul>
-                  @foreach ($errors->all() as $item)
-                  <li>{{$item}}</li>                      
-                  @endforeach
-              </div>
+            <div class="alert alert-danger">
+              <ul>
+                @foreach ($errors->all() as $item)
+                  <li>{{ $item }}</li>
+                @endforeach
+              </ul>
+            </div>
           @endif
           <div class="row">
             <div class="col-md-12 stretch-card">
@@ -108,43 +98,27 @@
             </div>
           </div>
         </div>
-        <!-- content-wrapper ends -->
-        <!-- partial:partials/_footer.html -->
         <footer class="footer">
-        <div class="d-sm-flex justify-content-center justify-content-sm-between">
-          <span class="text-muted text-center text-sm-left d-block d-sm-inline-block"> </a>Kelompok WP7</span>
-          <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"></a> Web Profile </span>
-        </div>
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Kelompok WP7</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Web Profile</span>
+          </div>
         </footer>
-        <!-- partial -->
       </div>
-      <!-- main-panel ends -->
     </div>
-    <!-- page-body-wrapper ends -->
   </div>
-  <!-- container-scroller -->
-
-  <!-- plugins:js -->
-  <script src="{{asset('admin')}}/vendors/base/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page-->
-  <script src="{{asset('admin')}}/vendors/chart.js/Chart.min.js"></script>
-  <script src="{{asset('admin')}}/vendors/datatables.net/jquery.dataTables.js"></script>
-  <script src="{{asset('admin')}}/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
-  <!-- End plugin js for this page-->
-  <!-- inject:js -->
-  <script src="{{asset('admin')}}/js/off-canvas.js"></script>
-  <script src="{{asset('admin')}}/js/hoverable-collapse.js"></script>
-  <script src="{{asset('admin')}}/js/template.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="{{asset('admin')}}/js/dashboard.js"></script>
-  <script src="{{asset('admin')}}/js/data-table.js"></script>
-  <script src="{{asset('admin')}}/js/jquery.dataTables.js"></script>
-  <script src="{{asset('admin')}}/js/dataTables.bootstrap4.js"></script>
-  <!-- End custom js for this page-->
-
-  <script src="{{asset('admin')}}/js/jquery.cookie.js" type="text/javascript"></script>
+  <script src="{{ asset('admin') }}/vendors/base/vendor.bundle.base.js"></script>
+  <script src="{{ asset('admin') }}/vendors/chart.js/Chart.min.js"></script>
+  <script src="{{ asset('admin') }}/vendors/datatables.net/jquery.dataTables.js"></script>
+  <script src="{{ asset('admin') }}/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+  <script src="{{ asset('admin') }}/js/off-canvas.js"></script>
+  <script src="{{ asset('admin') }}/js/hoverable-collapse.js"></script>
+  <script src="{{ asset('admin') }}/js/template.js"></script>
+  <script src="{{ asset('admin') }}/js/dashboard.js"></script>
+  <script src="{{ asset('admin') }}/js/data-table.js"></script>
+  <script src="{{ asset('admin') }}/js/jquery.dataTables.js"></script>
+  <script src="{{ asset('admin') }}/js/dataTables.bootstrap4.js"></script>
+  <script src="{{ asset('admin') }}/js/jquery.cookie.js" type="text/javascript"></script>
 </body>
 
 </html>
