3fa33483fbae863af0e15db20c766e22004d586a
agistadivab <agista.diva.tif22@polban.ac.id>
2023-11-28 10:51:51 +0700

    about

--- resources/views/depan/about.blade.php
+++ resources/views/depan/about.blade.php
@@ -13,37 +13,11 @@
     <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css">
     <link href="{{ asset('depan') }}/css/about.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <!-- Navigation -->
-    @if(session()->has('success'))
-        <div id="successAlert" class="alert alert-success text-center" role="alert">
-            {{ session('success') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-
-        <script>
-            // Setelah 2 detik, sembunyikan alert dengan efek fade out
-            setTimeout(function() {
-                var alert = document.getElementById('successAlert');
-                alert.style.transition = "opacity 1s";
-                alert.style.opacity = 0;
-
-                setTimeout(function() {
-                    alert.style.display = 'none';
-                }, 1000); // Waktu tambahan sebelum elemen benar-benar dihapus (sesuaikan sesuai kebutuhan)
-            }, 2000);
-        </script>
-    @endif
-
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="sideNav">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
             aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -58,14 +32,13 @@
             </ul>
         </div>
     </nav>
-
     <!-- Isi dari Cv -->
     <div class="container">
         <div class="profile">
             <div class="profile_container">
                 <div class="profile_profileImg">
                     <img class="images" src="{{ asset('storage/' . $data['profile']->gambar) }}" alt="profile" />
-                    <a href="{{ route('generate.pdf', $data['profile']->id) }}" class="downloadBtn"
+                    <a href="{{ route('generate.pdf', $data['profile']->id) }}" class="downloadBtn "
                         target="_blank">Download ATS CV</a>
                     @if($data['profile']->user_id === $data['currentId'])
                     <a href="{{ route('profile.edit', $data['profile']->id) }}" class="downloadBtn">Edit</a>
@@ -130,8 +103,7 @@
                 <!-- Container for Keterangan -->
                 <div class="keterangan-container">
                     <div class="title1">Keterangan :</div>
-                    <div class="text-keterangan"> 
-                        <p>Merah => Pemula</p>
+                    <div class="text-keterangan" <p>Merah => Pemula</p>
                         <p>Kuning => Menengah</p>
                         <p>Hijau => Mahir</p>
                     </div>
